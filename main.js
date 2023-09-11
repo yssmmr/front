5a93819108bb9cf98856ccd33b70898279a5e220
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-09-11 04:43:53 +0000

    Add コールバック関数

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ const hello = (name, age) =>{
 
 hello('Code Mafia, 10');
 hello('Code Mafia, 20');
+
+
+const arry = [1,2,3,4,5,6];
+arry.forEach(value => console.log(value));
+
+
+//コールバック関数
+
+function hello2(callback) {
+  console.log(callback);
+  console.log('hello' + callback());
+}
+
+
+function getName() {
+  return 'Code Mafia';
+}
+
+const getFirstName = function() {
+  return 'Code';
+}
+
+
+hello2(getFirstName);
